e2ce6e2dca27364f21a6447d43380c545e472555
luojunping <junping.luo@aqara.com>
2019-11-21 18:39:03 +0800

    airflow source code

---
+++ airflow/migrations/versions/7bc42538a698_.py
@@ -0,0 +1,45 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""empty message
+
+Revision ID: 7bc42538a698
+Revises: a56c9515abdc, 6e96a59344a4, 004c1210f153
+Create Date: 2019-11-13 15:23:16.213970
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7bc42538a698'
+down_revision = ('a56c9515abdc', '6e96a59344a4', '004c1210f153')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """Apply empty message"""
+    pass
+
+
+def downgrade():
+    """Unapply empty message"""
+    pass
